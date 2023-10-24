65dd6a0b041a7a7510cbbe46de891ca660bb5189
Jinma0 <94667828+Jinma0@users.noreply.github.com>
2023-10-23 22:00:22 -0400

    update

--- proj1/src/proj1/HeartRateDemo.java
+++ proj1/src/proj1/HeartRateDemo.java
@@ -6,27 +6,40 @@ import java.util.Scanner;
 public class HeartRateDemo {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Patient's first name: ");
-        String firstName = keyboard.nextLine();
 
-        System.out.println("Patient's last name: ");
-        String lastName = keyboard.nextLine();
+        boolean programOver = false;
 
-        System.out.println("Patient's birth month (MM): ");
-        int birthMonth = Integer.parseInt(keyboard.nextLine());
+        while (programOver = false) {
+            System.out.println("Patient's first name: ");
+            String firstName = keyboard.nextLine();
 
-        System.out.println("Patient's birth day (DD): ");
-        int birthDay = Integer.parseInt(keyboard.nextLine());
+            System.out.println("Patient's last name: ");
+            String lastName = keyboard.nextLine();
 
-        System.out.println("Patient's birth year (YYYY): ");
-        int birthYear = Integer.parseInt(keyboard.nextLine());
+            System.out.println("Patient's birth month (MM): ");
+            int birthMonth = Integer.parseInt(keyboard.nextLine());
 
-        DateOfBirth dateOfBirth = new DateOfBirth(birthYear, birthMonth, birthDay);
+            System.out.println("Patient's birth day (DD): ");
+            int birthDay = Integer.parseInt(keyboard.nextLine());
 
-        PersonHeartRate personHeartRate = new PersonHeartRate(firstName, lastName, dateOfBirth);
+            System.out.println("Patient's birth year (YYYY): ");
+            int birthYear = Integer.parseInt(keyboard.nextLine());
 
-        System.out.println(personHeartRate.maxHeartRate());
+            DateOfBirth dateOfBirth = new DateOfBirth(birthMonth, birthDay, birthYear);
+            PersonHeartRate personHeartRate = new PersonHeartRate(firstName, lastName, dateOfBirth);
+            ArrayList<PersonHeartRate> heartRateList = new ArrayList<>();
+            heartRateList.add(personHeartRate);
+        }
 
-        ArrayList<PersonHeartRate> heartRateList = new ArrayList<>();
+        System.out.println("Enter the number 1 to submit another patient, or enter 0 to continue...");
+        int userPrompt = Integer.parseInt(keyboard.nextLine());
+
+        if (userPrompt == 1){
+            programOver = false;
+        } else if (userPrompt == 0) {
+            programOver = true;
+        } else {
+            System.out.println("Enter the number 1 to submit another patient, or enter 0 to continue...");
+        }
     }
 }
--- proj1/src/proj1/PersonHeartRate.java
+++ proj1/src/proj1/PersonHeartRate.java
@@ -6,10 +6,7 @@ import java.time.Period;
 public class PersonHeartRate {
     String firstName;
     String lastName;
-    int month;
-    int day;
-    int year;
-    DateOfBirth birthDate = new DateOfBirth(year, day, month);
+    DateOfBirth birthDate;
 
     public PersonHeartRate(String firstName, String lastName, DateOfBirth birthDate){
         this.firstName = firstName;
@@ -33,42 +30,30 @@ public class PersonHeartRate {
         this.lastName = lastName;
     }
 
-    public int getMonth(){
-        return month;
-    }
+    public int calculateAge(){
+        LocalDate localDate = LocalDate.of(birthDate.getYear(),
+                birthDate.getMonth(),
+                birthDate.getDay());
 
-    public void setMonth(int month){
-        this.month = month;
-    }
-
-    public int getDay(){
-        return day;
-    }
-
-    public void setDay(int day){
-        this.day = day;
-    }
-
-    public int getYear(){
-        return year;
-    }
-
-    public void setYear(int year){
-        this.year = year;
-    }
-
-    public int calculateAge(int year, int month, int day){
-        LocalDate birthDate = LocalDate.of(getYear(), getMonth(),getDay());
         LocalDate todaysDate = LocalDate.now();
-        Period patientAge = Period.between(birthDate, todaysDate);
-        return Integer.parseInt(String.valueOf(patientAge));
+
+        Period patientAge = Period.between(localDate, todaysDate);
+        return patientAge.getYears();
     }
 
     public int maxHeartRate(){
-        return 220 - calculateAge(year, month, day);
+        return 220 - calculateAge();
     }
 
-    public int printData(){
-        return maxHeartRate();
+    public String targetHeartRateRange(){
+        int lowRange = maxHeartRate() / 2;
+        int highRange = (85 / maxHeartRate()) * 100;
+        return lowRange + " - " + highRange;
+    }
+
+    public void printData(){
+        System.out.println(lastName + ", " + firstName);
+        System.out.println("Max heart rate: " + maxHeartRate());
+        System.out.println("Target heart rate: " + targetHeartRateRange());
     }
 }
